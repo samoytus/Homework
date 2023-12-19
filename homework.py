da957c8971b928f529a148980c5fb76eed34f24e
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-19 18:58:31 +0000

    Исправления
    - Исправлена ошибка типа

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 class Animal:
-    def __init__(self, name, sex, age):
+    def __init__(self, name: str, sex: str, age: int):
         self.name = name
         self.sex = sex
         self.age = age
@@ -18,30 +18,39 @@ class Animal:
               f'Мой пол: {self.sex}. '
               f'Мне {self.age} лет!')
 
+
 class Cat(Animal):
     def run(self):
         print("Кошка бегает")
+
     def voice(self):
         print("Я умею мурлыкать")
+
     def eat(self):
         print("Я люблю молоко")
+
     def info(self):
         print(f"Привет, моя кличка {self.name}, я игривая!")
+
 
 class Dog(Animal):
     def run(self):
         print("Я бегаю быстрее всех")
+
     def voice(self):
         print("Громко рычу")
+
     def eat(self):
         print("Люблю кушать")
+
     def info(self):
         print(f"Привет, моя кличка {self.name}, я люблю своего хозяина!")
 
-my_cat = Cat("Мурка", "Женский", "4")
+
+my_cat = Cat("Мурка", "Женский", 4)
 my_cat.info()
 my_cat.voice()
-my_dog = Dog ("Боб", "Мужской", "10")
+
+my_dog = Dog("Боб", "Мужской", 10)
 my_dog.info()
 my_dog.run()
-
